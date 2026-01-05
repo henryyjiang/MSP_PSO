912680b6e59bcfe0a2da9678f485cbfbf6cf6036
henryyjiang <henryyjiang42@gmail.com>
2026-01-05 11:39:08 -0500

    new ciffs

--- scripts/pso_local.py
+++ scripts/pso_local.py
@@ -358,8 +358,8 @@ if __name__ == "__main__":
         cell = extract_cell(cif)
 
         options = {'c1': 1.5, 'c2': 1.5, 'w': 0.5}  # cognitive, social, inertia
-        particles = 20  # number of particles in system
-        iters = 100
+        particles = 10  # number of particles in system
+        iters = 50
         local_steps = 25
 
         cell_perturb = False
