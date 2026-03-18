65a0f44b75ebf4dc7c6148d13af2a0ddd20dbefc
Henry Jiang <henryyjiang42@gmail.com>
2026-03-18 10:06:47 -0400

    ready for pso test

--- scripts/pso_local.py
+++ scripts/pso_local.py
@@ -370,8 +370,8 @@ if __name__ == "__main__":
         cell = extract_cell(cif)
 
         options = {'c1': 1.2, 'c2': 1.2, 'w': 0.5}  # cognitive, social, inertia
-        particles = 30  # number of particles in system
-        iters = 100
+        particles = 40  # number of particles in system
+        iters = 50
         local_steps = 25
 
         cell_perturb = True
